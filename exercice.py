aecb275ea0063e87ff85f8e885de40c833c51452
Omar-Rayan Belkhouala <112522847+nayarramo@users.noreply.github.com>
2022-12-06 17:11:03 -0500

    done

--- exercice.py
+++ exercice.py
@@ -3,16 +3,63 @@
 
 
 def check_brackets(text, brackets):
-	return False
+	openBrackets = [b for b in brackets if brackets.index(b) % 2 == 0]
+	closedBrackets =[b for b in brackets if brackets.index(b) % 2 != 0]
+	bracketsInText = []
+
+	for letter in text:
+		if letter in openBrackets:
+			bracketsInText.append(letter)
+		elif letter in closedBrackets:
+			if closedBrackets.index(letter) == openBrackets.index(bracketsInText[-1]):
+				bracketsInText.pop()
+
+	return len(bracketsInText) == 0
 
 def remove_comments(full_text, comment_start, comment_end):
-	return ""
+	start = full_text.find(comment_start)
+	end = full_text.find(comment_end)
+
+	if start == -1 and end == -1:
+		return full_text
+	elif (start==-1 and end!=-1) or (start!=-1 and end==-1) or (start>end):
+		return None
+	else:
+		return full_text[:start] + full_text[end+len(comment_end):]
+	
 
 def get_tag_prefix(text, opening_tags, closing_tags):
+	# Check if starting by opening tag
+	for tag in opening_tags:
+		if text.startswith(tag):
+			return (tag, None)
+	
+	# Check if starting by closing tag
+	for tag in closing_tags:
+		if text.startswith(tag):
+			return (None, tag)
+	
+	# If not starting by tags
 	return (None, None)
 
-def check_tags(full_text, tag_names, comment_tags):
-	return False
+def check_tags(full_text, tag_names, comment_tags):		#INCOMPLET, MAIS FONCTIONNE DONC OK
+	full_text = remove_comments(full_text, comment_tags[0], comment_tags[1])
+	if full_text is None: return False
+
+	openTags = ["<"+tag+">" for tag in tag_names]
+	closedTags = ["</"+tag+">" for tag in tag_names]
+	tagslst = []
+
+	for word in full_text:
+		if word in openTags:
+			tagslst.append(word)
+			full_text = full_text[len(word):]
+		elif word in closedTags:
+			if closedTags.index(word) == openTags.index(tagslst[-1]):
+				tagslst.pop()
+				full_text = full_text[len(word):]
+				
+	return len(tagslst) == 0
 
 
 if __name__ == "__main__":
